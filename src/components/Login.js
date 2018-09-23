c8514210877fc49506eb50bcdd97a03e31910aaf
Chris <christocarr@outlook.com>
2018-09-23 22:11:15 +0100

    Add login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Login = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+class Login extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="login-container">
+          <div className="logo">LOGO</div>
+          <h2 className="login-header">LOGIN</h2>
+          <form>
+            <label>
+              Email
+              <input type="text" name="email" />
+            </label>
+            <label>
+              Password
+              <input type="text" name="password" />
+            </label>
+              <input type="submit" value="SIGN IN" className="login-button" />
+              <p className="create-account">Don't have an account? Click here</p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
