70a5f01fbfdc86882e9465b1e489d5920b351438
MrTrush95 <martin.truchon95@gmail.com>
2018-09-23 15:40:10 -0400

    Revert "Wip/login"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
-      {/*use switch as there will be many routes */}
-      <Switch>
-        <Route path="/login" component={Login} />
-      </Switch>
+      <h1>Album Front-End</h1>
     </div>
   );
 };
