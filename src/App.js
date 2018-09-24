a75f6847ddc3c8b43742644e022eb5865b32c093
RichardC32 <richard.chalmersseo@gmail.com>
2018-09-24 14:07:42 +1000

    Started adding in the colors and Form in. Let me know if this is correct as i've only briefly had time to look at it

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Login from './Components/Login';
 const App = () => {
   return (
     <div>
+    <div className = "Form">
+    <h1> Login </h1>
+    <input type="email" placeholder="Email"/><br/>
+    <input type="password" placeholder="Password" />
       <Switch>
         <Route path="/login" component={Login} />
       </Switch>
+    </div>
     </div>
   );
 };
